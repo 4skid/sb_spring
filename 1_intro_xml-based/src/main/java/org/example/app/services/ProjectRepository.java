98fe87590dec9a63b11758c5fe61bf1d84d67f71
Михаил Тукмаков <amgs630@gmail.com>
2023-02-03 13:39:12 +0300

    Initial commit

---
+++ 1_intro_xml-based/src/main/java/org/example/app/services/ProjectRepository.java
@@ -0,0 +1,16 @@
+package org.example.app.services;
+
+import java.util.List;
+
+public interface ProjectRepository<T> {
+    List<T> retreiveAll();
+
+    void store(T book);
+
+    boolean removeItemById(Integer bookIdToRemove);
+
+    boolean removeItemByRegex(String bookStringByRegex);
+
+    boolean removeItemBySize(Integer bookSize);
+
+}
